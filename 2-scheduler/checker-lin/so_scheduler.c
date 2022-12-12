df860047379d6c7047285b1205cc418b147d745a
Glodea Eusebiu <94383599+paxtheg@users.noreply.github.com>
2022-12-12 13:22:21 +0200

    Update so_scheduler.c

--- 2-scheduler/checker-lin/so_scheduler.c
+++ 2-scheduler/checker-lin/so_scheduler.c
@@ -288,148 +288,6 @@ int so_init(unsigned int time_quantum, unsigned int io)
 	return 0;
 }
 
-/*
-	Functie care introduce in scheduler un nou thread si
-ii planifica executia; intoarce id ul threadului
-*/
-tid_t so_fork(so_handler *func, unsigned int priority)
-{
-	Thread_t *thread;
-
-	if (priority > SO_MAX_PRIO || func == NULL)
-		return INVALID_TID;
-
-	thread = (Thread_t *) malloc(sizeof(Thread_t));
-	if (thread == NULL)
-		return INVALID_TID;
-
-	thread->priority = priority;
-	thread->state = NEW;
-	thread->time_remaining = scheduler->time_quantum;
-	thread->handler = func;
-	sem_init(&thread->is_planned, 0, 0);
-	sem_init(&thread->is_running, 0, 0);
-	(scheduler->threads_nr)++;
-
-	
-	if (pthread_create(&thread->id, NULL, thread_func, thread) != 0) {
-		free(thread);
-		return INVALID_TID;
-	}
-
-	sem_wait(&thread->is_planned);
-	/*asteapta pana ce threadul a fost executat*/
-
-	if (scheduler->running != thread)
-		so_exec();
-	/*executa o instructiune*/
-
-	return thread->id;
-}
-
-/*
-	Functie care blocheaza threadul pana cand primeste o 
-operatie de executat; intoarce 0 daca nu intampina vreo 
-eroare sau -1 in caz contrar
-*/
-int so_wait(unsigned int io)
-{
-
-	if (scheduler->io <= io)
-		return -1;
-
-	Thread_t *thread = scheduler->running;
-	thread->state = WAITING;
-	push_back(scheduler->waiting[io], thread);
-
-	scheduler->running = pop_front(scheduler->ready);
-	if (scheduler->running != NULL)
-		sem_post(&scheduler->running->is_running);
-	/*paseaza executia la urmatorul thread in starea READY*/
-
-	sem_wait(&thread->is_running);
-	/*asteapta pana cand se afla in starea RUNNING*/
-
-	return 0;
-}
-
-/*
-	Functia deblocheaza toate threadurile care asteapta IO ul corespunzator
-Intoarce numarul de threaduri deblocate daca nu intampina vreo 
-eroare sau -1 in caz contrar
-*/
-int so_signal(unsigned int io)
-{
-	if (io >= scheduler->io)
-		return -1;
-
-	Thread_t *thread = pop_front(scheduler->waiting[io]);
-	int cnt = 0;
-
-	while (thread != NULL) {
-		thread->state = READY;
-		push_back(scheduler->ready, thread);
-
-		cnt++;
-		thread = pop_front(scheduler->waiting[io]);
-	}
-
-	/*executa o instructiune*/
-	so_exec();
-	return cnt;
-}
-
-/*
-	Functia simuleaza executia unei instructiuni, scade quantum 
-in cazul unui thread in starea RUNNING si intrerupe 
-un thread daca este necesar
-*/
-void so_exec(void)
-{
-	if (scheduler->running == NULL)
-		return;
-
-	Thread_t *thread = scheduler->running;
-
-	thread->time_remaining--;
-	if (thread->time_remaining == 0) {
-		thread->state = READY;
-		thread->time_remaining = scheduler->time_quantum;
-		push_back(scheduler->ready, thread);
-		/* se muta threadul in starea READY in coada ready */
-
-		Thread_t *next_thread = pop_front(scheduler->ready);
-		/* urmatorul thread in starea READY care nu s-a terminat */
-
-		while (next_thread && next_thread->state == TERMINATED) {
-			push_back(scheduler->terminated, next_thread);
-			next_thread = pop_front(scheduler->ready);
-		}
-
-		scheduler->running = next_thread;
-		if (scheduler->running != NULL) {
-			/* se seteaza threadul in starea RUNNING */
-			scheduler->running->state = RUNNING;
-			sem_post(&scheduler->running->is_running);
-		}
-	} else if (queue_front(scheduler->ready) &&
-		thread->priority < 
-		((Thread_t *)queue_front(scheduler->ready))->priority) {
-		/* swap intre threadul curent in starea RUNNING si threadul 
-		cu cea mai mare prioritate*/
-		thread->state = READY;
-		push_back(scheduler->ready, thread);
-		scheduler->running = pop_front(scheduler->ready);
-		scheduler->running->state = RUNNING;
-		sem_post(&scheduler->running->is_running);
-	}
-
-	
-	/*daca un thread a fost intrerupt, se asteapta pana cand se 
-	intoarce in starea RUNNING*/
-	if (thread != scheduler->running)
-		sem_wait(&thread->is_running);	
-}
 
 /*
 	Functia elimina memoria aferenta threadului
@@ -472,68 +330,17 @@ void so_end(void)
 	scheduler = NULL;
 }
 
-/*
-	Functia planifica un nou thread si ii seteaza 
-starea pe RUNNING sau READY
-*/
-void plan_new_thread(Thread_t *thread)
-{
-	if (scheduler->running == NULL) {
-		thread->state = RUNNING;
-		scheduler->running = thread;
-		sem_post(&scheduler->running->is_running);
-		return;
-	}
-
-	if (thread->priority > scheduler->running->priority) {
-		Thread_t *aux = scheduler->running;
-		scheduler->running = thread;
-		thread->state = RUNNING;
-		aux->state = READY;
-		push_back(scheduler->ready, aux);
-		return;
-	}
-
-	thread->state = READY;
-	push_back(scheduler->ready, thread);
+tid_t so_fork(so_handler *func, unsigned int priority){
+	return INVALID_TID;
 }
 
-/* Functie care opreste un thread*/
-void thread_finished(void)
-{
-	Thread_t *thread = scheduler->running;
-
-	if (thread->state == TERMINATED) {
-		push_back(scheduler->terminated, thread);
-		scheduler->running = pop_front(scheduler->ready);
-
-		if (scheduler->running != NULL)
-			sem_post(&scheduler->running->is_running);
-
-		if (scheduler->running == NULL && queue_front(scheduler->ready) == NULL)
-			sem_post(&scheduler->stop);
-
-	}
-
+void so_exec(void){
 }
 
-/* Functie care organizeaza rutina threadurilor */
-void *thread_func(void *args)
-{
-	Thread_t *thread = (Thread_t *) args;
-
-	/* planifica threadul curent si seteaza is_planned */
-	plan_new_thread(thread);
-	sem_post(&thread->is_planned);
-
-	/* asteapta pana cand threadul se afla in starea RUNNING */
-	sem_wait(&thread->is_running);
-
-	thread->handler(thread->priority);
-	
-	thread->state = TERMINATED;
-	thread_finished();
-
-	return NULL;
+int so_wait(unsigned int io){
+	return 0;
 }
 
+int so_signal(unsigned int io){
+	return 0;
+}
